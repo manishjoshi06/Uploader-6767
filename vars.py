b12e0989c4f29967fc020f0f6f33d5ab207e2858
manishjoshi06 <manishjoshi7654@gmail.com>
2026-01-10 21:39:17 -0800

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,11 +2,11 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "23231900"))
-API_HASH = os.environ.get("API_HASH", "d030150e323a1ff50851db2044cfe6ad")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8252945981:AAHwlBW-CbZgXaU8s-dZoBlddivdt-WcnB8")
+API_ID = int(os.environ.get("API_ID", "25861636"))
+API_HASH = os.environ.get("API_HASH", "7cb06d66a4ad6407aff96477c15d86eb")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8397779510:AAH2wax2H2P_2hGhrWLiQeDENYQFVHPNYCA")
 
-CREDIT = os.environ.get("CREDIT", "BOBY PIPPAL")
+CREDIT = os.environ.get("CREDIT", "JOSHI MANISH")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "CpprivateApi")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipToKUEnmcp@cpprivateapi.ghhp3oz.mongodb.net/?appName=CpprivateApi")  # Add your own atlas db
@@ -59,6 +59,7 @@ Please contact the admin to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
